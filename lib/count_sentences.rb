79361ae68fb4d6d57aaa78bf6744450a5841717b
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-05 16:10:11 -0400

    tests pass

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with? "!"
 
   end
 
   def count_sentences
-
+    split_array = self.split(/[?.!]/)
+    split_array.each do |sentence|
+      if sentence.length == 0 then
+        split_array.delete(sentence)
+      end
+    end
+    split_array.length
+    #puts split_array
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
